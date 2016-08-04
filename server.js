3ae536c6796c5e7998d4cdbe82d4c00805a9275a
Patricio López <lopezjuripatricio@gmail.com>
2016-08-04 12:28:18 -0400

    allow return tree on express

--- server.js
+++ server.js
@@ -3,10 +3,16 @@
 const express = require('express')
 const app = express()
 
+const curriculums = require('./index')()
+
 const PORT = 8000
 
 app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get('/tree', function (req, res) {
+  res.send(curriculums)
 })
 
 app.use('/static', express.static(`${__dirname}/static`))
